7f3dd3888d429224a8db8e44cd675b2c4858f801
Zach Dennis <zach.dennis@gmail.com>
2026-03-07 09:55:35 -0500

    Add SimpleCov for line and branch coverage reporting
    Generates HTML and JSON reports to coverage/. The coverage/
    directory is gitignored.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source "https://rubygems.org"
 
 group :development do
   gem "rspec", "~> 3.13"
+  gem "simplecov", "~> 0.22"
+  gem "simplecov_json_formatter", "~> 0.1"
   gem "standard", "~> 1.44"
   gem "yard", "~> 0.9"
   gem "redcarpet", "~> 3.6"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,18 @@
+require "simplecov"
+require "simplecov_json_formatter"
+
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/bin/"
+
+  enable_coverage :branch
+
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::JSONFormatter
+  ])
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "workspace"
 
